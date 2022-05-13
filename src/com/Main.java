ab260d9866fa45ef18945bfe086779578df23718
Pain4pple <laranicole.gatchalian.cics@ust.edu.ph>
2022-05-13 15:25:12 +0800

    Update Main.java

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,7 +3,28 @@ package com;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub!");
+		System.out.println("Penklawan Vs Me\r\n"
+				+ "Penklawan: Magnanakaw naman yang BBM nyo!\r\n"
+				+ "Me:*sa isip lang, kasi may respeto ako* ha? Pinagsasasabi nito?\r\n"
+				+ "Pinklawan: Oo magnanakaw sya! Wla pang diploma\r\n"
+				+ "Me: *tinaasan ng kilay. *sa isip* pigilan nyo ako!\r\n"
+				+ "Penklawan: HAHA MARCOS magnanakaw !\r\n"
+				+ "Me: *Napikon tas tumayo* Ha? Oo magnanakaw talaga siya!\r\n"
+				+ "MAGNANAKAW stands for..\r\n"
+				+ "M- Mannered\r\n"
+				+ "A-Admirable\r\n"
+				+ "G-Gifted\r\n"
+				+ "N- iNtelligent\r\n"
+				+ "A-Amazing\r\n"
+				+ "N-nice one baby\r\n"
+				+ "A-Amusing\r\n"
+				+ "K-Kind\r\n"
+				+ "A-Agreeable\r\n"
+				+ "W-Worldclass\r\n"
+				+ "Ano may laban k pa ba?.. Ha? Tsaka walang diploma? May ebidensya ka? May ibidinsya ka..? Sus inggit ka lng... e puro kayo paninira!!\r\n"
+				+ "Penklawan: *Nagwalk out kasi hindi kinaya ang kahihiyan\r\n"
+				+ "Nakikinig: Ooohhhh savage!!\r\n"
+				+ "Me: Hmm, maliit na bagay, mga ka bbm");
 	}
 
 }
