c7fb32efb7a87479038689e0580ac8d85c7bb3c5
Pain4pple <laranicole.gatchalian.cics@ust.edu.ph>
2022-05-25 09:40:56 +0800

    A New Start
    changes:
    gwacalc.java now extends Jframe
    usrlogin.java now extends JPanel
    removed jframe variable "f" and jpanel variable "userLoginPanel"
    moved the font variables as classes can only extend one class
    changed setSize to setPreferredSize
    
    recommendation:
    use interfaces to hold font variables or any similar gui customization (e.g. background color) so that classes can implement it without any limitation

---
+++ src/com/GwaCalc.java
@@ -0,0 +1,45 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GwaCalc extends JFrame{
+	
+	private JFrame f;
+	private UsrLogin usrlog;
+	
+	Font tahomaPlain16 = new Font("Tahoma", Font.PLAIN, 16);
+	Font tahomaBold16 = new Font("Tahoma", Font.BOLD, 16);
+	Font tahomaPlain12 = new Font("Tahoma", Font.PLAIN, 12);
+	Font tahomaBold12 = new Font("Tahoma", Font.BOLD, 12);
+	
+	// Color _ = new Color();
+	
+	public GwaCalc() {
+		
+		setLayout(null);
+		setPreferredSize(new Dimension(1280, 720));
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+		// to resolve auto focus on username textfield
+//		inv = new JTextField("");
+//		inv.requestFocus(); 
+//		f.add(inv);
+		usrlog = new UsrLogin();
+		setContentPane(usrlog);
+		repaint();
+		revalidate();
+		
+		pack();
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		GwaCalc gwa = new GwaCalc();
+	}
+
+}
