21a3f80aa4d245952eda450aba2f5074b10760c2
kigos21 <83705521+kigos21@users.noreply.github.com>
2022-05-26 18:58:31 +0800

    Update UsrCreate.java

--- src/com/gui/UsrCreate.java
+++ src/com/gui/UsrCreate.java
@@ -10,8 +10,9 @@ public class UsrCreate extends guiCustoms {
 	
 	Font gothamBook, gothamBookBold;
 	JPanel usrCreate;
-	JLabel lLogo;
-	JLabel lCreateTtl;
+	JLabel lLogo, lCreateTtl, lUser, lPswrd;
+	JTextField tfUsrnm, tfPswrd;
+	JPasswordField pfPswd;
 	
 	public UsrCreate() {
 		try {
@@ -44,7 +45,69 @@ public class UsrCreate extends guiCustoms {
 		lCreateTtl.setForeground(Color.WHITE);
 		usrCreate.add(lCreateTtl);
 		
+		lUser = new JLabel("USERNAME");
+		lUser.setBounds(476, 298, 81, 15);
+		lUser.setFont(gothamBook.deriveFont(Font.PLAIN, 13));
+		lUser.setHorizontalAlignment(JTextField.LEFT);
+		lUser.setForeground(Color.WHITE);
+		usrCreate.add(lUser);
 		
+		tfUsrnm = new JTextField("Username");
+		tfUsrnm.setBounds(455, 319, 371, 47);
+		tfUsrnm.setFont(gothamBook.deriveFont(Font.PLAIN,16));
+		tfUsrnm.setHorizontalAlignment(JTextField.CENTER);
+		tfUsrnm.setForeground(textfieldGray);
+		tfUsrnm.addFocusListener(new FocusListener() {
+		    public void focusGained(FocusEvent e) {
+		    	tfUsrnm.setText("");
+		    }
+
+		    public void focusLost(FocusEvent e) {
+		    	if (tfUsrnm.getText().isBlank())
+		    		tfUsrnm.setText("Username");
+		    }
+		});
+		usrCreate.add(tfUsrnm);
 		
+		lPswrd = new JLabel("PASSWORD");
+		lPswrd.setBounds(476, 372, 191, 15);
+		lPswrd.setFont(gothamBook.deriveFont(Font.PLAIN,13));
+		lPswrd.setHorizontalAlignment(JTextField.LEFT);
+		lPswrd.setForeground(Color.WHITE);
+		usrCreate.add(lPswrd);
+		
+		tfPswrd = new JPasswordField("Password");
+		tfPswrd.setBounds(455, 393, 371, 47);
+		tfPswrd.setFont(gothamBook.deriveFont(Font.PLAIN,16));
+		tfPswrd.setHorizontalAlignment(JTextField.CENTER);
+		tfPswrd.setForeground(textfieldGray);
+		tfPswrd.addFocusListener(new FocusListener() {
+		    public void focusGained(FocusEvent e) {
+		    	tfPswrd.setText("");
+		    }
+
+		    public void focusLost(FocusEvent e) {
+		        if (tfPswrd.getText().isBlank())
+		        	tfPswrd.setText("Password");
+		    }
+		});
+		usrCreate.add(tfPswrd);
+		
+//		tfCnfPswrd = new JPasswordField("Password");
+//		tfPswrd.setBounds(455, 393, 371, 47);
+//		tfPswrd.setFont(gothamBook.deriveFont(Font.PLAIN,16));
+//		tfPswrd.setHorizontalAlignment(JTextField.CENTER);
+//		tfPswrd.setForeground(textfieldGray);
+//		tfPswrd.addFocusListener(new FocusListener() {
+//		    public void focusGained(FocusEvent e) {
+//		    	tfPswrd.setText("");
+//		    }
+//
+//		    public void focusLost(FocusEvent e) {
+//		        if (tfPswrd.getText().isBlank())
+//		        	tfPswrd.setText("Password");
+//		    }
+//		});
+//		usrCreate.add(tfPswrd);
 	}
 }
